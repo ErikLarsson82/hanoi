d7cfcc43998c56a3fe6173ae5c718bb62ecc440c
Erik Larsson <erik.mattias.larsson@gmail.com>
2017-09-24 19:47:55 +0200

    Cleaned comments and code

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-/*function hanoi(disc, src, aux, dst) {
+/*
+//Syncronous version
+function hanoi(disc, src, aux, dst) {
     if (disc <= 0) return
 
     hanoi(disc - 1, src, dst, aux)
@@ -6,8 +8,14 @@
     hanoi(disc - 1, aux, src, dst)
 }
 
-hanoi(3, "Src", "Aux", "Dst")*/
+hanoi(3, "Src", "Aux", "Dst")
+*/
 
+
+
+
+
+//Asyncronous version
 let canvas, ctx, discs
 const pos = {
     "Src": 0,
@@ -15,38 +23,22 @@ const pos = {
     "Dst": 200,
 }
 
-
 function asyncHanoi(disc, src, aux, dst, anim) {
-    console.log('Attempt schedule Disc ' + disc + ' move to ' + src + ' via ' + aux + ' to ' + dst)
-
-    if (disc <= 0) return Promise.resolve('so done')
+    if (disc <= 0) return Promise.resolve()
 
     return asyncHanoi(disc - 1, src, dst, aux, anim)
-        .then(response => {
-            console.log('next in line is prepped', disc, response)
-
-            //let disc = disc - 1
+        .then(() => {
             return new Promise(resolve => {
-                console.log('Schedules anim ' + disc)
                 anim({ disc, src, aux, dst }, pos[dst], resolve.bind(null, { disc, src, aux, dst }))
             })
-            .then(response => {
-                console.log('animation done hook', disc, response)
-                
+            .then(() => {
                 return asyncHanoi(disc - 1, aux, src, dst, anim)
-                    .then(response => {
-                        console.log('just wanted to tell ya that you are cool', response)
-                        return 'returning more'
-                    })
-                //return 'an heres another'
             })
-
-            //return 'and who recieves this'
-        })  /*, src, aux, dst, anim*/
+        })
 }
 
 function animationLib(params, x, callback) {
-    TweenLite.to(discs[params.disc-1], 1, { x: x, onUpdate:redraw, onComplete: callback })
+    TweenLite.to(discs[params.disc-1], 0.1, { x: x, onUpdate:redraw, onComplete: callback })
 }
 
 function init(amount) {
@@ -61,8 +53,6 @@ function init(amount) {
     redraw()
 
     asyncHanoi(amount, "Src", "Aux", "Dst", animationLib)
-        .then(console.log.bind(null, 'Im done'))
-
 }
 
 const rectHeight = 20
@@ -76,9 +66,9 @@ function redraw() {
     _.each(discs, (item, idx) => {
         ctx.save()
         ctx.fillRect(
-            discs[idx].x, // + (discs.length - idx) * 5,
+            discs[idx].x,
             canvas.height - (rectHeight * discs.length) + (rectHeight * idx),
-            100, //Math.max(100 / (discs.length - idx), 10),
+            100,
             rectHeight
         )
     })
